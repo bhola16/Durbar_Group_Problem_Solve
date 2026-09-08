132323ead894c13fa2fd1fb72e674d3e606e5c8a
bhola16 <ku.bhola16@gmail.com>
2026-09-08 23:48:27 +0600

    Add Contest 3 solutions

---
+++ JS/p1.js
@@ -0,0 +1,12 @@
+function reverseEachWord(str) {
+
+    const newStr = str.split(" ")
+    const reverseStr = newStr.map(word => word.split("").reverse().join(""))
+    const returnStr = reverseStr.join(" ");
+
+    return returnStr;
+
+}
+
+console.log(reverseEachWord("Hello World"));
+console.log(reverseEachWord("JavaScript is fun"));
